4d07df5aa3f23c2798289371a1fa5cfdfc10a9b2
ss13 <ssa0618@yandex.ru>
2023-08-23 14:40:13 +0300

    delete idea

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,61 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.util.Locale;
+
+public class DataHelper {
+    private static Faker faker = new Faker(new Locale("en"));
+
+    private DataHelper() {
+    }
+
+    public static AuthInfo getAuthInfo() {
+
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    private static String generateRandomLogin() {
+
+        return faker.name().username();
+    }
+
+    private static String generateRandomPassword() {
+
+        return faker.internet().password();
+    }
+
+    public static AuthInfo generateRandomUser() {
+        return new AuthInfo(generateRandomLogin(), generateRandomPassword());
+    }
+    public static VerificationCode generateRandomCode() {
+
+        return new VerificationCode(faker.numerify("######"));
+    }
+
+    @Value
+    public static class AuthInfo {
+       private String login;
+       private String password;
+    }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AuthCode {
+        private String id;
+        private String user_id;
+        private String code;
+        private String created;
+    }
+
+}
